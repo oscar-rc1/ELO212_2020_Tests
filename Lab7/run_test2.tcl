75ef14ec2ccbe99545798ee1a4862da340b01e68
Oscar Rojas <oscar@oscar-rc.dev>
2020-07-27 11:15:15 -0400

    [Lab7] Agregar autotest para Parte 2

---
+++ Lab7/run_test2.tcl
@@ -0,0 +1,15 @@
+cd [file dirname [info script]]
+
+create_project -force Sesion7_ELO212_Test2 -part xc7a100tcsg324-1 vivado/test1
+
+read_verilog -sv tb/tb_act2.sv
+read_verilog -sv ../Modules/calc_fsm.sv
+read_verilog -sv ../ExtModules/ALU_ref/ALU_ref.sv
+read_verilog -sv [glob ../ExtModules/PushButton-debouncer/project_1.srcs/sources_1/new/*.sv]
+read_verilog -sv [glob dut/*.sv]
+
+set_property -name "top" -value "tb_act2" -objects [get_filesets sim_1]
+set_property -name "xsim.simulate.runtime" -value "1s" -objects [get_filesets sim_1]
+
+update_compile_order -fileset sources_1
+launch_simulation
